fc0b48d20773c7542ec29411c36bb311fb7eba5c
Orfeas Liossatos <oiliossatos@gmail.com>
2023-10-03 16:12:49 +0200

    Renamed file. Improvements.
    Now operates on one model at a time. More modular with activation and architecture arguments, and delete option for results file.

--- Tasks/tr_size_vs_te_acc.py
+++ Tasks/tr_size_vs_te_acc.py
@@ -1,6 +1,7 @@
 # Implementation of the experiments described in Week1-FCNNvsCNN
 
 # Optional Arguments
+# -actv (str) The name of the activation function to use
 # -min/max (int) The min/max order of magnitude for trainset size. Default 2/4.
 # -s (int) The number training sample sets (of exponentially increasing size). Default 5.
 # -f (str) The output .json file name (must contain an empty {}). Default week1_out.json
@@ -10,22 +11,22 @@
 
 # Imports
 import sys
+import pickle
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
 from torchsummary import summary
-from json import dump, load
 
 # Local python scripts
-from helpers import roll_avg_rel_change, calc_label, numel, printProgressBar
-from models import CNN, DCNN, FCNN, Quadratic
+from helpers import roll_avg_rel_change, calc_label, numel, printProgressBar, strToBool
+from models import ModelLoader
 
 # Read terminal input
 args = sys.argv[1:]
 arg_pairs=zip(args[::2],args[1::2])
-valid_options = dict([('-min', int), ('-max', int), ('-s', int), ('-f', str), ('-e', int), ('-p', int), ('-l', int)])
+valid_options = dict([('-actv', str), ('-arch', str), ('-min', int), ('-max', int), ('-s', int), ('-f', str), ('-e', int), ('-p', int), ('-l', int), ('-del', strToBool)])
 
 # Save options here
 option_dict = {}
@@ -48,7 +49,6 @@ try:
 except ValueError:
     print(ValueError)
     
-      
 # Use GPU if available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("Using device: ", device, "\n")
@@ -56,6 +56,9 @@ print("Using device: ", device, "\n")
 # Seed random number generation
 torch.manual_seed(0)
 np.random.seed(0)
+
+# Model loader
+loader = ModelLoader()
 
 # Global constants
 learning_rate = 0.01
@@ -88,24 +91,26 @@ p_norm = option_dict.get('-p') or 2
 gauss_y_tr = calc_label(gauss_x_tr, p=p_norm).to(device)
 gauss_y_te = calc_label(gauss_x_te, p=p_norm).to(device)
 
+# Option del - delete contents of results file
+file_path = option_dict.get('-f') or 'week1_out.pkl'
+if option_dict.get('-del') or False:
+    with open(file_path, 'wb+') as file:
+        pickle.dump(dict(), file)
+
 # Print empty progress bar
 printProgressBar(0, splits, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
 # Train models on various input sizes
 for k, N in enumerate(Ns):
     
-    printProgressBar(k + 1, splits, prefix = 'Progress:', suffix = 'Complete', length = 50)
-    
     # Models
-    models = []
-    for arch in [CNN, DCNN, FCNN]:
-        for activation in [nn.ReLU(), Quadratic()]:
-            models += [arch(input_shape, activation).to(device)]
-            
-    names = ["CNN+ReLU", "CNN+Quad","DCNN+ReLU", "DCNN+Quad", "FCNN+ReLU","FCNN+Quad"]
+    architecture = option_dict.get('-arch') or "FCNN"
+    activation = option_dict.get('-actv') or "ReLU"
+    model = loader.load(architecture, activation, input_shape=input_shape)    
+    name = architecture + "+" + activation
     
     # Optimizers
-    optimizers = [optim.SGD(model.parameters(), lr=learning_rate) for model in models]
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
     criterion = nn.BCELoss()
     
     # Create a DataLoader for dataset
@@ -113,49 +118,47 @@ for k, N in enumerate(Ns):
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     # Training loop
-    for i, model in enumerate(models):
-        optimizer = optimizers[i]
-        model.train()
-        epoch = 0
-        converged = False
-        queue = []
-        while not converged:
-            for batch_x, batch_y in dataloader:
-                optimizer.zero_grad()
-                output = model(batch_x)
-                loss = criterion(output, batch_y)
-                loss.backward()
-                optimizer.step()
+    model.train()
+    epoch = 0
+    converged = False
+    queue = []
+    while not converged:
+        for batch_x, batch_y in dataloader:
+            optimizer.zero_grad()
+            output = model(batch_x)
+            loss = criterion(output, batch_y)
+            loss.backward()
+            optimizer.step()
 
-                # Is it converged?
-                roll_avg = roll_avg_rel_change(queue, window, loss.item())
-                if (roll_avg and roll_avg < rel_conv_crit and loss < abs_conv_crit) or epoch == max_epochs:
-                    converged = True
-                    break
+            # Is it converged?
+            roll_avg = roll_avg_rel_change(queue, window, loss.item())
+            if (roll_avg and roll_avg < rel_conv_crit and loss < abs_conv_crit) or epoch == max_epochs:
+                converged = True
+                break
 
-            epoch+=1
-        
-        # print(names[i], "finished. \tEpoch:", epoch,"\tLoss: %.2f" % loss.item(), "\tRoll: %.2f" % roll_avg)
+        epoch += 1
+    
+    # print(names[i], "finished. \tEpoch:", epoch,"\tLoss: %.2f" % loss.item(), "\tRoll: %.2f" % roll_avg)
 
     # Evaluate models
-    test_loss = [0 for i in range(len(models))]
-    accuracy = [0 for i in range(len(models))]
-    
+    test_loss = 0
+    accuracy = 0
     with torch.no_grad():
-        for i, model in enumerate(models):
-            model.eval()
-            out = model(gauss_x_te)
-            test_loss[i] += criterion(out, gauss_y_te)
-            accuracy[i] += float(sum(torch.eq((out>0.5).to(float),gauss_y_te))/N_te)
+        model.eval()
+        out = model(gauss_x_te)
+        test_loss += criterion(out, gauss_y_te)
+        accuracy += float(sum(torch.eq((out>0.5).to(float),gauss_y_te))/N_te)
 
-    # Read the JSON file
-    file_path = option_dict.get('-f') or 'week1_out.json'
-    with open(file_path, 'r') as json_file:
-        test_acc = load(json_file)
+    # Read file contents
+    with open(file_path, 'rb') as file:
+        test_acc = pickle.load(file)
 
     # Add experiment to results
-    test_acc[str(N)]=accuracy
+    test_acc[(name, N)] = accuracy
 
     # Write accuracy to file
-    with open(file_path, 'w') as json_file:
-        dump(test_acc, json_file)
+    with open(file_path, 'wb') as file:
+        pickle.dump(test_acc, file)
+
+    # Print progress
+    printProgressBar(k + 1, splits, prefix = 'Progress:', suffix = 'Complete', length = 50)
