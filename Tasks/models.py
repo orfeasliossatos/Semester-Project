9bdca10817655ee97633cf12537e0c3f5f0500e9
Orfeas Liossatos <oiliossatos@gmail.com>
2023-10-03 16:07:57 +0200

    Renamed DNN. Added ModelLoader
    The ModelLoader class lets you load a model configuration with arbitrary parameters.

--- Tasks/models.py
+++ Tasks/models.py
@@ -5,13 +5,13 @@ from helpers import numel
 
 # Define CNN
 class CNN(nn.Module):
-    def __init__(self, input_shape, activation):
+    def __init__(self, activation, **kwargs):
         super(CNN, self).__init__()
         
         self.conv = nn.Conv2d(3, 100, kernel_size=3, stride=1, padding=1)
         self.activation = activation
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc = nn.Linear(numel(input_shape, [self.conv,self.pool]), 1)
+        self.fc = nn.Linear(numel(kwargs['input_shape'], [self.conv,self.pool]), 1)
         self.sigmoid = nn.Sigmoid()
         
     def forward(self, x):
@@ -24,13 +24,13 @@ class CNN(nn.Module):
         return x
     
 # Define 'dumb' CNN
-class DCNN(nn.Module):
-    def __init__(self, input_shape, activation):
-        super(DCNN, self).__init__()
+class DumbCNN(nn.Module):
+    def __init__(self, activation, **kwargs):
+        super(DumbCNN, self).__init__()
         self.conv = nn.Conv2d(3, 2, kernel_size=1, stride=1, padding=0)
         self.activation = activation
-        self.pool = nn.AvgPool2d(input_shape[1])
-        self.fc = nn.Linear(numel(input_shape, [self.conv,self.pool]), 1)
+        self.pool = nn.AvgPool2d(kwargs['input_shape'][1])
+        self.fc = nn.Linear(numel(kwargs['input_shape'], [self.conv,self.pool]), 1)
         self.sigmoid = nn.Sigmoid()
         
     def forward(self, x):
@@ -44,11 +44,11 @@ class DCNN(nn.Module):
     
 # Define FCNN
 class FCNN(nn.Module):
-    def __init__(self, input_shape, activation):
+    def __init__(self, activation, **kwargs):
         super(FCNN, self).__init__()
         
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(prod(input_shape),3072)
+        self.fc1 = nn.Linear(prod(kwargs['input_shape']),3072)
         self.activation = activation
         self.fc2 = nn.Linear(3072, 1)
         self.sigmoid = nn.Sigmoid()
@@ -66,3 +66,19 @@ class FCNN(nn.Module):
 class Quadratic(nn.Module):
     def forward(self, x):
         return x**2
+
+# Loads models from strings
+class ModelLoader:
+    def __init__(self):
+        self.architectures = dict(zip(["CNN", "DumbCNN", "FCNN"], [CNN, DumbCNN, FCNN]))
+        self.activations = dict(zip(["ReLU", "Quad"],[nn.ReLU(), Quadratic()]))
+
+    def load(self, architecture, activation, **kwargs):
+        """
+        Loads an architecture with options.
+        @params:
+            architecture    - Required : name of the architecture (Str)
+            activation      - Required : name of the activation function (Str)
+            input_shape     - Optional : shape of a single input (Tuple)
+        """
+        return self.architectures[architecture](self.activations[activation], **kwargs)
