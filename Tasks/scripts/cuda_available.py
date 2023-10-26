249d516a2872f838e2bd6f72e097ab7b418c74d1
Orfeas Ioannis Liossatos <oliossat@lth.epfl.ch>
2023-10-26 14:07:53 +0200

    Removed test and output files

--- Tasks/scripts/cuda_available.py
+++ Tasks/scripts/cuda_available.py
@@ -3,3 +3,9 @@ if torch.cuda.is_available():
     print("Cuda is available")
 else:
     print("Cuda is not available")
+
+print("Torch version : ", torch.__version__)
+
+print(torch.zeros(1).cuda())
+
+
